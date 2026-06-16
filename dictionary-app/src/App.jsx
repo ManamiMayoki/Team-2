fabab949c563fe067724950428336abac2a81329
ManamiMayoki <manamimarufa@gmail.com>
2026-06-16 23:14:40 +0600

    counter

--- dictionary-app/src/App.jsx
+++ dictionary-app/src/App.jsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 function Counter(){
   const [count, setCount] = useState(0);
 
-  return {
-    }
+
+return (
+  <button onClick={() => setCount(count + 1)}>
+    Clicked {count} times
+  </button>
+);
+
 }
+
+export default Counter;
