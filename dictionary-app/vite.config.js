95eb3672fd61564e3b1e2ad75dbd67ef3bc918dd
ManamiMayoki <manamimarufa@gmail.com>
2026-06-16 22:26:38 +0600

    tailwindcss plugin

--- dictionary-app/vite.config.js
+++ dictionary-app/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'//import tailwindcss plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],//add tailwindcss plugin to the list of plugins
 })
