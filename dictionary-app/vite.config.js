c8f0a2278be3242924ea6b6c6e8a8505b5e94f24
ManamiMayoki <manamimarufa@gmail.com>
2026-06-16 22:45:36 +0600

    themes

--- dictionary-app/vite.config.js
+++ dictionary-app/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'//import tailwindcss plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],//add tailwindcss plugin to the list of plugins
+  plugins: [react(), tailwindcss()],//add tailwindcss plugin to the list of
 })
